6466c6b1c54bb248a705835b034cd4c558e7ce7d
Clemens Quoss <clemens@quoss.de>
2025-02-01 14:27:57 +0100

    Update App.java: add put-message function

--- mq-project-client/src/main/java/de/quoss/mq/client/App.java
+++ mq-project-client/src/main/java/de/quoss/mq/client/App.java
@@ -2,13 +2,21 @@ package de.quoss.mq.client;
 
 import com.ibm.mq.jms.MQQueueConnectionFactory;
 import com.ibm.msg.client.wmq.WMQConstants;
+
+import java.util.Arrays;
 import java.util.Enumeration;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
+import javax.jms.JMSProducer;
+import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.QueueBrowser;
 import javax.jms.QueueConnection;
 import javax.jms.QueueSession;
 import javax.jms.Session;
+import javax.jms.TextMessage;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -33,6 +41,7 @@ public class App {
     final String useridValue;
     
     public App(final String[] args) {
+        LOGGER.trace("init start [args={}]", Arrays.asList(args));
         final Options options = new Options();
         final Option channelOption = Option.builder("c")
                 .longOpt("channel")
@@ -102,6 +111,7 @@ public class App {
         queueManagerValue = commandLine.getOptionValue("qm");
         typeValue = commandLine.getOptionValue("t");
         useridValue = commandLine.getOptionValue("u");
+        LOGGER.trace("init end");
     }
     
     public static void main(final String[] args) {
@@ -110,7 +120,39 @@ public class App {
         LOGGER.trace("main end");
     }
 
+    private Connection connection(final ConnectionFactory factory) throws JMSException {
+        LOGGER.trace("connection start");
+        final Connection result;
+        if (passwordValue == null || useridValue == null) {
+            throw new MqClientException("Invalid credentials provided");
+        }
+        result = factory.createConnection(useridValue, passwordValue);
+        LOGGER.trace("connection end [result={}]", result);
+        return result;
+    }
+
+    private ConnectionFactory connectionFactory() {
+        LOGGER.trace("connection-factory start");
+        final MQQueueConnectionFactory result = new MQQueueConnectionFactory();
+        try {
+            result.setChannel(channelValue);
+            if (hostNameValue != null) {
+                result.setHostName(hostNameValue);
+            }
+            if (portValue != null) {
+                result.setPort(Integer.parseInt(portValue));
+            }
+            result.setQueueManager(queueManagerValue);
+            result.setTransportType(WMQConstants.WMQ_CM_CLIENT);
+        } catch (final JMSException e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+        LOGGER.trace("connection-factory end [result={}]", result);
+        return result;
+    }
+
     private void listMessageIds() {
+        LOGGER.trace("list-message-ids start");
         if (null == typeValue) {
             throw new MqClientException("No type value provided");
         } else {
@@ -119,54 +161,76 @@ public class App {
                 default -> throw new MqClientException("Type value '" + typeValue + "' not supported");
             }
         }
+        LOGGER.trace("list-message-ids end");
     }
 
     private void listMessageIdsJms() {
-        final MQQueueConnectionFactory factory = new MQQueueConnectionFactory();
-        try {
-            factory.setChannel(channelValue);
-            if (hostNameValue != null) {
-                factory.setHostName(hostNameValue);
-            }
-            if (portValue != null) {
-                factory.setPort(Integer.parseInt(portValue));
-            }
-            factory.setQueueManager(queueManagerValue);
-            factory.setTransportType(WMQConstants.WMQ_CM_CLIENT);
-        } catch (final JMSException e) {
-            throw new IllegalStateException(e.getMessage(), e);
-        }
-        if (passwordValue == null || useridValue == null) {
-            throw new MqClientException("Invalid credentials provided");
-        }
-        try (final QueueConnection connection = factory.createQueueConnection(useridValue, passwordValue);
-                final QueueSession session = connection.createQueueSession(true, Session.AUTO_ACKNOWLEDGE)) {
-            LOGGER.trace("list-message-ids-jms [connection={},session={}]", connection, session);
+        LOGGER.trace("list-message-ids-jms start");
+        final ConnectionFactory factory = connectionFactory();
+        try (final Connection connection = connection(factory);
+             final Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE)) {
+            LOGGER.debug("list-message-ids-jms [connection={},session={}]", connection, session);
             final Queue queue = session.createQueue(queueValue);
             final QueueBrowser browser = session.createBrowser(queue);
             final Enumeration<?> enumeration = browser.getEnumeration();
             int i = 0;
             while (enumeration.hasMoreElements()) {
                 final Object o = enumeration.nextElement();
-                LOGGER.trace("list-message-ids-jms [o={}]", o);
+                LOGGER.debug("list-message-ids-jms [o.class.name={},o={}]", o.getClass().getName(), o);
                 i++;
             }
-            LOGGER.trace("list-message-ids-jms [i={}]", i);
+            LOGGER.debug("list-message-ids-jms [i={}]", i);
             browser.close();
         } catch (final JMSException e) {
             throw new IllegalStateException(e.getMessage(), e);
         }
+        LOGGER.trace("list-message-ids-jms end");
     }
-    
+
+    private void putMessage() {
+        LOGGER.trace("put-message start");
+        if (null == typeValue) {
+            throw new MqClientException("No type value provided");
+        } else {
+            switch (typeValue) {
+                case "jms" -> putMessageJms();
+                default -> throw new MqClientException("Type value '" + typeValue + "' not supported");
+            }
+        }
+        LOGGER.trace("put-message end");
+    }
+
+    private void putMessageJms() {
+        LOGGER.trace("put-message-jms start");
+        final ConnectionFactory factory = connectionFactory();
+        try (final Connection connection = connection(factory);
+             final Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE)) {
+            LOGGER.debug("put-message-jms [connection={},session={}]", connection, session);
+            final Queue queue = session.createQueue(queueValue);
+            final TextMessage message = session.createTextMessage();
+            message.setText("foo");
+            try (final MessageProducer producer = session.createProducer(queue)) {
+                producer.send(message);
+            }
+            session.commit();
+        } catch (final JMSException e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+        LOGGER.trace("put-message-jms end");
+    }
+
     private void run() {
+        LOGGER.trace("run start");
         if (null == functionValue) {
             throw new MqClientException("No function value provided");
         } else {
             switch (functionValue) {
                 case "list-message-ids" -> listMessageIds();
+                case "put-message" -> putMessage();
                 default -> throw new MqClientException("Function value '" + functionValue + "' not supported");
             }
         }
+        LOGGER.trace("run end");
     }
 
 }
